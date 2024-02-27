800b8882b5a52e465021bce6a511927cc3f1afc5
영선 <sunnyskylove22@gmail.com>
2024-02-27 17:09:09 +0900

    [24.02.27] 입출력

---
+++ src/main/java/com/ohgiraffers/section01/file/Application.java
@@ -0,0 +1,41 @@
+package com.ohgiraffers.section01.file;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        /* 수업목표. File 클래스의 사용 용법을 이해할 수 있다. */
+        /* 필기.
+        *   파일 처리를 수행하는 대표적인 클래스이다.
+        *   대상 파일에 대한 정보로 인스턴스를 생성하고
+        *   파일의 생성, 삭제 등등의 처리를 수행하는 기능을 제공하고 있다.
+        * */
+
+        File file = new File("src/main/java/com/ohgiraffers/section01/file/test.txt");
+        // path 안에는 경로를 적어준다.
+
+        try {
+            boolean creatSuccess = file.createNewFile();
+
+            System.out.println("creatSuccess : " + creatSuccess);
+            // true이면서 하위의 txt 파일 생성됨
+            // >>true > false(2번째 생성부터 나옴)
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("파일의 크기 : " + file.length() + "byte");
+        System.out.println("파일의 경로 : " + file.getPath());
+        System.out.println("파일의 상위 경로 : " + file.getParent());
+        System.out.println("파일의 절대 경로 : " + file.getAbsoluteFile());
+
+        // 파일의 삭제(삭제 성공 후 성공 실패 여부를 boolean 타입으로 반환한다.)
+        boolean deleteSuccess = file.delete();
+        System.out.println("deleteSuccess = " + deleteSuccess);         // App 하위의 txt 파일 없어진다.
+    }
+
+}
---
+++ src/main/java/com/ohgiraffers/section02/stream/Application02.java
@@ -0,0 +1,59 @@
+package com.ohgiraffers.section02.stream;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Application02 {
+
+    public static void main(String[] args) {
+
+        /* 수업목표. FileReader 를 이해하고 사용할 수 있다. */
+        /* 필기.
+         *   FileReader
+         *   FileInputStream 과 사용하는 방법이 거의 동일하다.
+         *   단, byte 단위가 아닌 character, 단위로 읽어들이는 부분이 차이점이다.
+         * */
+
+        // testReader 와 연결~~
+        FileReader fr = null;
+
+        try {
+            fr = new FileReader("src/main/java/com/ohgiraffers/section02/stream/testReader.txt");
+
+//            int value;
+//            while ((value = fr.read()) != -1) {
+//
+//                System.out.println((char) value);
+//
+//            }
+
+            char[] carr = new char[(int)new File("src/main/java/com/ohgiraffers.section02/stream/testReader.txt").length()];
+
+            fr.read(carr);
+
+            for(int i = 0; i <carr.length; i++) {
+                System.out.println(carr[i]);         // 한번 읽힌 건 재사용이 안되기 떄문에, 위의 int value~while 밑까지 주석처리!
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+
+        }
+
+        finally {
+            if(fr != null){
+
+                try {
+                    fr.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
